86c7ede04c6ed600cbcdb71b3df4e78cba7bc7aa
David Kabath <dvkb@mms-t-systems.de>
2020-10-29 23:35:03 +0100

    Updates

---
+++ precache-manifest.11cc7e5cc63d166d4f5e4a262f48f37e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2ebf426706023244a897673b3df9143",
+    "url": "/investice-byt/index.html"
+  },
+  {
+    "revision": "2f48eec7782c8fdfa635",
+    "url": "/investice-byt/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1715fd61adc4aeaa5dfc",
+    "url": "/investice-byt/static/css/main.0af6acd7.chunk.css"
+  },
+  {
+    "revision": "2f48eec7782c8fdfa635",
+    "url": "/investice-byt/static/js/2.997ea755.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/investice-byt/static/js/2.997ea755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1715fd61adc4aeaa5dfc",
+    "url": "/investice-byt/static/js/main.30777d65.chunk.js"
+  },
+  {
+    "revision": "ebf0ba2168a826fcf30a",
+    "url": "/investice-byt/static/js/runtime-main.f5a308af.js"
+  }
+]);
